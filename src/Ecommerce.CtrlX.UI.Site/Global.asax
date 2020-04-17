3e9d64405f1b7a974dfda28a07665de4c15aa5d3
Carol Domingues <caroldomingues13@gmail.com>
2020-04-16 22:37:14 -0300

    Adicionado o projeto UI.Site

---
+++ src/Ecommerce.CtrlX.UI.Site/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Ecommerce.CtrlX.UI.Site.MvcApplication" Language="C#" %>
---
+++ src/Ecommerce.CtrlX.UI.Site/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Ecommerce.CtrlX.UI.Site.Startup))]
+namespace Ecommerce.CtrlX.UI.Site
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
