db07f4f703820fb9eb599ee0c8cc78518459c19d
Carol Domingues <carol@novatela.com.br>
2020-05-15 18:06:26 -0300

    Adicionado repository e config - Products

---
+++ src/Ecommerce.CtrlX.Infra.Data/EntityConfig/ProductsConfig.cs
@@ -0,0 +1,20 @@
+﻿using Ecommerce.CtrlX.Domain.Entities;
+using System.Data.Entity.ModelConfiguration;
+
+namespace Ecommerce.CtrlX.Infra.Data.EntityConfig
+{
+    public class ProductsConfig : EntityTypeConfiguration<Products>
+    {
+        public ProductsConfig()
+        {
+            HasKey(p => p.ProductId);
+            Property(p => p.Description).IsOptional().HasMaxLength(150);
+            Property(p => p.BarCode).IsOptional();
+            Property(p => p.Price).IsOptional();
+            Property(p => p.Image).IsOptional();
+            Property(p => p.Remarks).IsOptional();
+
+            ToTable("CtrlX_Products");
+        }
+    }
+}
---
+++ src/Ecommerce.CtrlX.Infra.Data/Repository/ProductsRepository.cs
@@ -0,0 +1,13 @@
+﻿using Ecommerce.CtrlX.Domain.Entities;
+using Ecommerce.CtrlX.Domain.Interfaces.Repository;
+using Ecommerce.CtrlX.Infra.Data.Context;
+
+namespace Ecommerce.CtrlX.Infra.Data.Repository
+{
+    public class ProductsRepository : Repository<Products>, IProductsRepository
+    {
+        public ProductsRepository(CtrlXContext context) : base(context)
+        {
+        }
+    }
+}
