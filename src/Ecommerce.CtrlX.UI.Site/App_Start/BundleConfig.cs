4a4412d67a823e4e8b357f8f1cf7f03fdc9e6aea
Carol Domingues <caroldomingues13@gmail.com>
2020-05-28 22:49:00 -0300

    correcoes

--- src/Ecommerce.CtrlX.UI.Site/App_Start/BundleConfig.cs
+++ src/Ecommerce.CtrlX.UI.Site/App_Start/BundleConfig.cs
@@ -26,6 +26,21 @@ namespace Ecommerce.CtrlX.UI.Site
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
 
+            bundles.Add(new StyleBundle("~/Content/TemmplateCss").Include(
+                      "~/Content/bootstrap.css",
+                      "~/Content/template/font-awesome.css",
+                      "~/Content/template/owl.carousel.css",
+                      "~/Content/template/responsive.css"
+                      ));
+
+            bundles.Add(new ScriptBundle("~/bundles/TemmplateJs").Include(
+                      "~/Scripts/template/bxslider.js",
+                      "~/Scripts/template/jquery.easing.1.3.js",
+                      "~/Scripts/template/jquery.sticky.js",
+                      "~/Scripts/template/owl.carousel.js",
+                      "~/Scripts/template/script.slider.js"
+                      ));
+
             bundles.Add(new ScriptBundle("~/bundles/products").Include(
                       "~/Scripts/jquery.mask.js",
                       "~/Scripts/Forms/products.js"));
